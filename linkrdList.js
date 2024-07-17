f7fe6944dc3bf5e16a1271b778e47192df9869a0
rafay <rafay356@gmail.com>
2024-07-17 17:36:19 +0500

    first commit

---
+++ linkrdList.js
@@ -0,0 +1,29 @@
+// class ListNode {
+//   constructor(data) {
+//     console.log(data, 'data');
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedList {
+//   constructor(head = null) {
+//     console.log(head, 'head');
+//     this.head = head;
+//   }
+// }
+
+// let node1 = new ListNode(2);
+// let node2 = new ListNode(5);
+// let node3 = new ListNode(19);
+
+// node1.next = node2;
+// node2.next = node3;
+
+// let list = new LinkedList(node2);
+
+// if (list.head?.next?.data === undefined) {
+//   console.log('data is null or undefined');
+// } else {
+//   console.log(list.head.next.data);
+// }
